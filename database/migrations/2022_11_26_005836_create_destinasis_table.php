d781d897de0690f29c09c0fce8c619639e787456
Hegel <hegelalrafli@gmail.com>
2022-11-26 08:20:53 +0700

    create migration destinasi and tiket

---
+++ database/migrations/2022_11_26_005836_create_destinasis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinasis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_destinasi');
+            $table->string('lokasi');
+            $table->text('deskripsi');
+            $table->integer('jml_view');
+            $table->integer('estimasi_harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destinasis');
+    }
+};
---
+++ database/migrations/2022_11_26_010327_create_tikets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_destinasi');
+            $table->string('tujuan');
+            $table->dateTime('dipesan');
+            $table->date('tanggal_wisata');
+            $table->enum('metode_bayar', ['Cash', 'Transfer']);
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tikets');
+    }
+};
